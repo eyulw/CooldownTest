5db72b214f6f1e082acd4e6be0094175c0259863
minha <alsgk6989@gmail.com>
2023-07-17 17:07:51 +0900

    Initial commit

---
+++ src/main/java/team/project/cooldown/CoolDownApplication.java
@@ -0,0 +1,13 @@
+package team.project.cooldown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolDownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/project/cooldown/CoolDownApplicationTests.java
@@ -0,0 +1,13 @@
+package team.project.cooldown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
